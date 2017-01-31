62e90ae6774f9ea00f247fa1a15b9b43239f9584
johnsontaiwo <josseytee@gmail.com>
2017-01-30 19:19:18 -0500

    creates database for contractor model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128003813) do
+ActiveRecord::Schema.define(version: 20170131001303) do
+
+  create_table "contractors", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "tax_id"
+    t.integer  "telephone"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
